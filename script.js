b69e91acde52f777bb50a69fdb29751fd580ee8a
marira <rongrazzo@gmail.com>
2021-05-15 22:47:53 +0600

    circle created by js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-window.addEventListener("DOMContentLoaded", (event) => {});
+window.addEventListener("DOMContentLoaded", (event) => {
+  createAllCircle();
+});
+
+function createCircle(deg) {
+  let div = document.createElement("div");
+  div.className = "line-1 line";
+  div.style = `transform: translate(-50%, -50%) rotate(` + deg + `deg)`;
+  return div;
+}
+
+function createAllCircle() {
+  for (let i = 0; i < 180; i += 5) {
+    let div = createCircle(i);
+    document.getElementById("center").append(div);
+  }
+}
